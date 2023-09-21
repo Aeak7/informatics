51bfc8a4fb5def843f5ebd54cb77eba23fc861be
Abigail Kelly <akelly17@huskers.unl.edu>
2023-09-21 15:20:01 -0500

    testing breaking segment

--- proj2/test.py
+++ proj2/test.py
@@ -1,13 +1,22 @@
-Lyrics = ["I want to to be like this", "I say hi to this"]
-Titles = ["Like", "Say"]
+lyrics = ["I want to to be like this", "I say hi to this"]
+titles = ["Like this", "Say hi this"]
 
 enter = str(input("Enter:\n"))
 
 found = []
 count = 0
-for lyric in Lyrics:
-  found.append(lyric.count(enter))
+for lyric in lyrics:
+  if enter in lyric:
+    count = lyric.count(enter)
+  #   newLyric = lyric.split()
+  #   for lyric in newLyric:
+  #     if enter == lyric:
+  #       found.append(lyric)
+  #       count += 1
+  # else:
+  #   print("no")
 
 print(found)
+print(count)
 
 
