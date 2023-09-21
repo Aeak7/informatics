f069b9a81cb79af5eab6326d421942207a4b353d
Abigail Kelly <akelly17@huskers.unl.edu>
2023-09-21 18:59:58 -0500

    Added in-line comments

--- proj2/functions.py
+++ proj2/functions.py
@@ -1,3 +1,6 @@
+# runs if option 1 is selected, loops though range of titles
+# makes sure all results are lowercase to prevent case-sensitivity
+# places and returns title and number of occurrences in a dictionary
 def optionOne(keyInput, songTitles):
   output = dict()
   for i in range(0, len(songTitles)-1):
@@ -6,7 +9,9 @@ def optionOne(keyInput, songTitles):
       output[songTitles[i].strip()] = result
   return output
   
-
+# runs if option 2 is selected, loops though range of lyrics
+# makes sure all results are lowercase to prevent case-sensitivity
+# places and returns title and number of occurrences in a dictionary
 def optionTwo(keyInput, songLyrics, songTitles):
   output = dict()
   for i in range(0, len(songLyrics)-1): 
--- proj2/main.py
+++ proj2/main.py
@@ -2,7 +2,7 @@
 # This program reads from an input text file, processes
 # the text lines to count the number of characters in the text,
 # and prompts user to enter keywords to count the number of
-# text lines that a keyword appears in.
+# times that a keyword appears in.
 #
 # The input file name is "songs.txt"
 #
@@ -10,19 +10,16 @@
 # Date : 09/19/2023
 # ---------------------------------------------------
 from functions import *
-import operator
 
 songTitles = []  # list to store all the songs' titles
 songLyrics = []  # list to store all the songs' lyrics
 
-inputFilename = 'songs.txt' # input file
+inputFilename = 'proj2/songs.txt' # input file
 
-# -- Given code
 # -----------------
 # Read in the input file
 # Store in the arrays/lists called songTitles and songLyrics
 # -----------------
-
 count = 0   # to keep track of the number of songs
 titleFlag = 1  # a flag to indicate that we are ready to read in the title of the next song
 lyricsSoFar = ""  # the temporary variable to hold all the lines of lyrics read in so far for a song
@@ -44,43 +41,45 @@ with open(inputFilename, "r") as infile:
 infile.close()
 
 
-# -- Start of added code
+# Welcomes user and explains parameters
 print("Welcome to the Text Analysis Program!")
 print("This program finds the songs that a search key is found in, using the input file 'songs.txt'")
 
 keyInput = 0
-while keyInput != -1:
+while keyInput != -1: # constant loop until user enters -1 to end
    keyInput = input("Please enter a search key(or -1 to exit):\n")
-   if keyInput == str(-1):
-      break;
+   if keyInput == str(-1): # logic to check if end while loop condition is met
+      break # break out of while
    else:
-      print("You entered: " + keyInput)
+      print("You entered: " + keyInput) # Tells user what word they entered
       print("Please select where you would like to search (1, 2, or 3):\n1) Within a song's title\n2) Within a song's lyrics\n3) Within both the title and lyrics")
-      selectionInput = input()
+      selectionInput = input() 
       if selectionInput == str(1): # search in songs title
-         results = optionOne(keyInput, songTitles)
-         for key, value in results.items():
+         results = optionOne(keyInput, songTitles) # calls function from functions.py
+         if len(results) == 0: # checks if result dictionary is empty 
+            print("There are no song titles containing this search key.")
+         for key, value in results.items(): # retrieves both key and value values from the dictionary to print
             print(f"{key}: Appears {value} times")
-         continue
+         continue # go back to start of while to continue
       elif selectionInput == str(2): # search in songs lyrics
-         results = optionTwo(keyInput, songLyrics, songTitles)
-         for key, value in results.items():
+         results = optionTwo(keyInput, songLyrics, songTitles) # calls function from functions.py
+         if len(results) == 0: # checks if result dictionary is empty 
+            print("There are no song lyrics containing this search key.")
+         for key, value in results.items(): # retrieves both key and value values from the dictionary to print
             print(f"{key}: Appears {value} times")
          continue
       elif selectionInput == str(3): # search both in titles and lyrics
-         print("Option 3") 
-         titles = optionOne(keyInput, songTitles)
+         titles = optionOne(keyInput, songTitles) # calls both previous functions from functions.py
          lyrics = optionTwo(keyInput, songLyrics, songTitles)
-         output = {x: titles.get(x, 0) + lyrics.get(x, 0) for x in set(titles).union(lyrics)}
-         for key, value in output.items():
+         output = {x: titles.get(x, 0) + lyrics.get(x, 0) for x in set(titles).union(lyrics)} # adds values of both dictionaries together to "search in both"
+         if len(output) == 0: # checks if result dictionary is empty 
+            print("There are no song titles or lyrics containing this search key.")
+         for key, value in output.items(): # retrieves both key and value values from the dictionary to print
             print(f"{key}: Appears {value} times")
+         continue
       else:
-         print("Sorry this input is invalid")
-         continue;
+         print("Sorry this input is invalid") # if user enters any out of 1, 2, or 3
+         continue
 
 # print out exit message
 print("Thank you for using the Text Analyzer!")
-       
-
-
-
