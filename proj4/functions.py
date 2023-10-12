e7b82a02e46eb7796ea6d10b69bdf324be50f89e
Abigail Kelly <akelly17@huskers.unl.edu>
2023-10-11 21:06:38 -0500

    Finished proj4

--- proj4/functions.py
+++ proj4/functions.py
@@ -1,6 +1,50 @@
+def modifyImage(choice, numRows, numCols, imageArray, highIntensity, average):
 # inverts the colors of the image, useful to view the image from a different perspective for finding discrepancies, similar to viewing an xray image
-def invertImageColors(numRows, numCols, highIntensity, imageArray):
-  for x in range(0, numRows - 1):
-      for y in range(0, numCols - 1):
-         imageArray[x][y] = highIntensity - imageArray[x][y]
+  if choice == "invert":
+    for x in range(0, numRows - 1):
+        for y in range(0, numCols - 1):
+          imageArray[x][y] = highIntensity - imageArray[x][y]
+    print(f"--> Inverted.")
+# subtracts the avg from the default, making the image significantly darker, perfect for spooky images
+  elif choice == "darken": 
+    for x in range(0, numRows - 1):
+        for y in range(0, numCols - 1):
+          imageArray[x][y] = imageArray[x][y] - average
+    print(f"--> Darkened.")
+  # the opposite of darken, adds the avg, making the image much easier to see on dimmer screens, particularly useful for my terribly dim monitor
+  elif choice == "lighten": 
+    for x in range(0, numRows - 1):
+        for y in range(0, numCols - 1):
+          imageArray[x][y] = imageArray[x][y] + average
+    print(f"--> Lightened.")
+  else:
+    print("Invalid Input")
 
+def readInFile(filename, imageArray):
+    # -- 1. read the image header
+  inputFile = open('informatics/proj3/images/'+filename+'.pgm','rb')  # open up the input file so we can read from it
+  inputFile.readline()  # read the comment line #1
+  inputFile.readline()  # read the comment line #2
+  (width, height) = [int(i) for i in inputFile.readline().split()]
+  depth = int(inputFile.readline())
+  # -- 2. read in the image, one byte at a time    
+  for y in range(height):
+      row = []
+      for y in range(width):
+          row.append(ord(inputFile.read(1)))
+      imageArray.append(row) 
+  inputFile.close()  # close the file
+
+def writeOutFile(filename, imageArray):
+  numRows = len(imageArray)
+  numCols = len(imageArray[0])
+  # -- 1. write out the image header
+  outputFile = open('informatics/proj3/images/'+filename+'out.pgm','w')
+  outputFile.write("P2\n")
+  outputFile.write("# output image\n")
+  outputFile.write("" + str(numRows) + " " + str(numCols) + "\n")
+  outputFile.write("255\n")
+  # -- 2. write out the image
+  for x in range(0,numRows):
+    for y in range(0,numCols):
+        outputFile.write(""+str(imageArray[x][y])+" ")
--- proj4/main.py
+++ proj4/main.py
@@ -5,7 +5,7 @@
 # It also prompts the user for the input filename to be used.
 #
 # Edited by Abby Kelly
-# September 25, 2022
+# October 11, 2023
 # Note:  It imports the math library in order to use the sqrt() 
 #        function
 # ---------------------------------------------------------------
@@ -14,35 +14,16 @@ import math
 import copy
 from functions import *
 
-# ----------------------------------------------
-# print out welcome message
-# ----------------------------------------------
+# entrance message and first input
 print("Welcome to the Image Statistical Analysis and Processing program.")
-
-# ----------------------------------------------
-# prompt the user for the input filename
-# ----------------------------------------------
 filename = input("Please enter an input filename (without the .pgm): ")
 
 # ----------------------------------------------
 # open and read from the input image file
-# store the image into the 2D array 'raster'
+# stores the image into the 2D array 'raster'
 # ----------------------------------------------
-# -- 1. read the image header
-inputFile = open('proj3/images/'+filename+'.pgm','rb')  # open up the input file so we can read from it
-inputFile.readline()  # read the comment line #1
-inputFile.readline()  # read the comment line #2
-(width, height) = [int(i) for i in inputFile.readline().split()]
-depth = int(inputFile.readline())
-
-# -- 2. read in the image, one byte at a time    
-raster = []  # image stored here
-for y in range(height):
-    row = []
-    for y in range(width):
-        row.append(ord(inputFile.read(1)))
-    raster.append(row) 
-inputFile.close()  # close the file
+raster = []
+readInFile(filename, raster)
 
 # check if input file is read in correctly, set up rows and columns
 numRows = len(raster)
@@ -91,45 +72,15 @@ print(f"stdev of intensity: {stdev}")
 # ----------------------------------------------
 # Modification of Image
 # ----------------------------------------------
-# Added an input question so each effect isn't interfering with the others
-# Similar to the copy created for the stdev so it wouldn't affect these effects
 choice = input("Please select an effect for your image (invert, darken, lighten): ")
-if choice == "invert":
-   invertImageColors(numRows, numCols, maxVal, raster)
-   print(f"--> Inverted. Find image at: {filename}out.pgm")
-# subtracts the avg from the default, making the image significantly darker, perfect for spooky images
-elif choice == "darken": 
-   for x in range(0, numRows - 1):
-      for y in range(0, numCols - 1):
-         raster[x][y] = raster[x][y] - average 
-   print(f"--> Darkened. Find image at: {filename}out.pgm")
-# the opposite of darken, adds the avg, making the image much easier to see on dimmer screens, particularly useful for my terribly dim monitor
-elif choice == "lighten": 
-   for x in range(0, numRows - 1):
-      for y in range(0, numCols - 1):
-         raster[x][y] = raster[x][y] + average
-   print(f"--> Lightened. Find image at: {filename}out.pgm")
-else:
-   print("Invalid Input")
+modifyImage(choice, numRows, numCols, raster, maxVal, average)
 
 # ----------------------------------------------
 # open and write to the output image file
 # ----------------------------------------------
-# -- 1. write out the image header
-outputFile = open('proj3/images/'+filename+'out.pgm','w')
-outputFile.write("P2\n")
-outputFile.write("# output image\n")
-outputFile.write("" + str(numRows) + " " + str(numCols) + "\n")
-outputFile.write("255\n")
+writeOutFile(filename, raster)
 
-# -- 2. write out the image
-for x in range(0,numRows):
-   for y in range(0,numCols):
-      outputFile.write(""+str(raster[x][y])+" ")
-
-# ----------------------------------------------           
-# Display an exit message
-# ----------------------------------------------
+# exiting message
 print("Thank you for using the Image Statistical Analysis and Processing program.")
 
 
